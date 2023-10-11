fa21e492dc1b9c55c049289569097a3f63a97cc1
Daan Knoors <dk@daanknoors.com>
2023-10-11 18:11:43 +0200

    upgrade dython to 0.6.8

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='synthetic_data_generation',
-    version='0.1.11',
+    version='0.1.12',
     description='Algorithms for generating synthetic data',
     author='Daan Knoors',
     author_email='d.knoors@gmail.com',
-    packages=find_packages(include=['synthesis']),
+    packages=find_packages(),
     install_requires=[
         'diffprivlib>=0.5.1',
         'dill>=0.3.4',
-        'dython==0.6.7',
+        'dython==0.6.8',
         'joblib>=1.0.1',
         'lifelines>=0.26.0',
         'matplotlib>=3.4.3',
--- synthesis/__init__.py
+++
@@ -1,3 +0,0 @@
-from synthesis import synthesizers
-from synthesis import transformers
-from synthesis import evaluation
